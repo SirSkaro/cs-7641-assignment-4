7132127cd823c1d4549312b3d5ad7852a8adc1a8
unknown <bencbchurchill@gmail.com>
2023-04-01 13:52:16 -0700

    Fixed bad matching in .gitignore

---
+++ src/main/java/edu/gatech/churchill/cs7641/gridworld/GridWorldAnalysis.java
@@ -0,0 +1,13 @@
+package edu.gatech.churchill.cs7641.gridworld;
+
+import burlap.behavior.policy.Policy;
+import burlap.behavior.singleagent.Episode;
+import burlap.behavior.singleagent.auxiliary.valuefunctionvis.ValueFunctionVisualizerGUI;
+
+public class GridWorldAnalysis {
+
+    public Policy policy;
+    public Episode episode;
+    public ValueFunctionVisualizerGUI gui;
+
+}
